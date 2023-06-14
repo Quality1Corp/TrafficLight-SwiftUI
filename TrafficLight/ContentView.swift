8bcb9640f1a2c3ce68f599c06ef2997e8f8e350d
Mikhail Mishurenkov <m.mishurenkov@yandex.ru>
2023-06-14 17:44:06 +0300

    work on logic of switching traffic lights

--- TrafficLight/ContentView.swift
+++ TrafficLight/ContentView.swift
@@ -7,15 +7,62 @@
 
 import SwiftUI
 
+enum CurrentLight {
+    case off, red, yellow, green
+}
+
 struct ContentView: View {
+    @State private var buttonTitle = "START"
+    @State private var currentLight: CurrentLight = .off
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.black.ignoresSafeArea()
+            
+            VStack(spacing: 20) {
+                ColorCircleView(
+                    color: .red,
+                    opacity: currentLight == .red ? 1 : 0.3
+                )
+                ColorCircleView(
+                    color: .yellow,
+                    opacity: currentLight == .yellow ? 1 : 0.3
+                )
+                ColorCircleView(
+                    color: .green,
+                    opacity: currentLight == .green ? 1 : 0.3
+                )
+                
+                Spacer()
+                
+                Button(action: nextColor) {
+                    Text(buttonTitle)
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                }
+                .frame(width: 200, height: 60)
+                .background(.blue)
+                .cornerRadius(20)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color.white, lineWidth: 4)
+                )
+            }
+            .padding()
         }
-        .padding()
+    }
+    
+    private func nextColor() {
+        switch currentLight {
+            case .off: currentLight = .red
+            case .red: currentLight = .yellow
+            case .yellow: currentLight = .green
+            case .green: currentLight = .red
+        }
+        if buttonTitle == "START" {
+            buttonTitle = "NEXT"
+        }
     }
 }
 
