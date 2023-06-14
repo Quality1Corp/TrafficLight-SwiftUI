fe25e82cd47101b19b380a53c7cb7d33da4116db
Mikhail Mishurenkov <m.mishurenkov@yandex.ru>
2023-06-14 15:36:55 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by MikhaiL on 14.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
